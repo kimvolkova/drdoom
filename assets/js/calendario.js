8b22a22bb4696763d6a70d33c73e3bdebfccc01d
Jose <josebayona201@gmail.com>
2024-10-28 10:00:36 -0500

    Add assets folder

---
+++ assets/js/calendario.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'));
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      locale: 'es',
+      headerToolbar: {
+        left: 'prev,next,today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,listWeek'
+      },
+      dateClick: function (info){
+        document.getElementById('fecha').value = info.dateStr;
+        myModal.show();
+      }
+    });
+    calendar.render();
+  });
+
